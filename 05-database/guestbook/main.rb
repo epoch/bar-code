8458c92c44abd4b4c71b415c3368642439939b98
Daniel Tsui <tsui.daniel@gmail.com>
2016-10-24 14:46:46 +1100

    guestbook

--- 05-database/guestbook/main.rb
+++ 05-database/guestbook/main.rb
@@ -4,13 +4,23 @@ require 'pg'
 
 get '/' do
   db = PG.connect(dbname: 'guestbook')
-
   @guests = db.exec('SELECT * FROM guests;')
-
   db.close
   erb :index
 end
 
-get '/checkin' do
+get '/about' do
+  'about me'
+end
 
+post '/checkin' do
+
+  db = PG.connect(dbname: 'guestbook')
+
+  # insert a new record
+  sql = "insert into guests (name, email, comment) values ('#{ params[:name] }', '#{ params[:email] }', '#{ params[:comment] }');"
+
+  db.exec(sql);
+  db.close
+  redirect to '/'
 end
