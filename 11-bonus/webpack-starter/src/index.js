007b6edf875ac31c8bd8a3f52705c4181207674d
Daniel Tsui <tsui.daniel@gmail.com>
2016-12-05 11:55:45 +1100

    php

---
+++ 11-bonus/webpack-starter/src/index.js
@@ -0,0 +1,13 @@
+console.log('yay webpack');
+
+console.log('sdsdfsd');
+
+const bam = () => {
+  console.log('bam');
+}
+
+bam();
+
+// var $ = require('jquery');
+import $ from 'jquery'
+console.log($.fn.jquery);
